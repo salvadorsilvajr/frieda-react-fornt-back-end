5ec6f191593eacc8e1a18859c48bcd8f5a68089d
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2020-07-06 00:07:17 -0500

    set alert  reducer done L-39

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import About from './components/layout/About';
-import Login from './components/auth/Login';
-import './App.css';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import About from "./components/layout/About";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 
 // redux
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
 
 const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<Header />
-				<Alert />
-				<Route exact path='/' component={About} />
-				<Switch>
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/register' component={Register} />
-				</Switch>
-				<Footer />
-			</Fragment>
-		</Router>
-	</Provider>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Header />
+        <Route exact path='/' component={About} />
+        <Alert />
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+        <Footer />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const Alert = ({ alerts }) =>
-	alerts !== null &&
-	alerts.length > 0 &&
-	alerts.map((alert) => (
-		<div className='container'>
-			<div key={alert.id} className={`m-a alert alert-${alert.alertType}`}>
-				<div className='p-a'>{alert.msg}</div>
-			</div>
-		</div>
-	));
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div key={alert.id} className={`m-a alert alert-${alert.alertType}`}>
+      <div className='p-a'>{alert.msg}</div>
+    </div>
+  ));
 
 Alert.propTypes = {
-	alerts: PropTypes.array.isRequired
+  alerts: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	alerts: state.alert
+  alerts: state.alert,
 });
 
 export default connect(mapStateToProps)(Alert);

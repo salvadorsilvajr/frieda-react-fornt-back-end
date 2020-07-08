6a8e08aa39bc2fc2f91e38098870187ba4308352
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2020-07-07 21:56:01 -0500

    register a database done w alerts L40

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFomrData] = useState({
     name: "",
     lastname: "",
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("passwords do not match", "danger");
     } else {
-      console.log(formData);
+      register({ name, lastname, email, phone, password });
     }
   };
 
@@ -133,6 +134,7 @@ const Register = ({ setAlert }) => {
 };
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -6,7 +6,11 @@ const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) => (
-    <div key={alert.id} className={`m-a alert alert-${alert.alertType}`}>
+    <div
+      key={alert.id}
+      className={`alert alert-${alert.alertType}`}
+      style={{ margin: "auto" }}
+    >
       <div className='p-a'>{alert.msg}</div>
     </div>
   ));

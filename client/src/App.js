63330d9899c29ccd3773acc896f29f1dd2e99ce2
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2020-07-08 20:42:08 -0500

    register and auth done L-41 send data mongo DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
@@ -10,24 +10,36 @@ import Alert from "./components/layout/Alert";
 // redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Header />
-        <Route exact path='/' component={About} />
-        <Alert />
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-        </Switch>
-        <Footer />
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Header />
+          <Route exact path='/' component={About} />
+          <Alert />
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+          <Footer />
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
